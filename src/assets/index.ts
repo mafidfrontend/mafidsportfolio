ff29f66ef25581804e028043ec3422e08e2a0047
mafidfrontend <neo.uz001@gmail.com>
2025-05-03 11:17:50 +0500

    portfolio website

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -14,10 +14,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.jpg";
-import tesla from "./company/tesla.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -38,10 +35,7 @@ export {
   redux,
   tailwind,
   typescript,
-  meta,
-  shopify,
   starbucks,
-  tesla,
   carrent,
   jobit,
   tripguide,
