8569438a9462f66d1efd15a8f2b42980b65d5487
mafidfrontend <neo.uz001@gmail.com>
2025-05-13 17:13:41 +0500

    .

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: process.env.NODE_ENV !== 'production',
-  }
-});
